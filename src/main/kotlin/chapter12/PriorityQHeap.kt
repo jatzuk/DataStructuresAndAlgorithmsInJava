a805934efc9a8bd44c29c62e2bbde67c25288f1d
jatzuk <jatzuk.22@gmail.com>
2019-05-16 12:05:21 +0300

    ch12 ex3

---
+++ src/main/kotlin/chapter12/PriorityQHeap.kt
@@ -0,0 +1,20 @@
+package chapter12
+
+/* 
+ * Created with passion and love
+ *    for project DataStructuresAndAlgorithmsInJava(Lafore)
+ *        by Jatzuk on 16.05.2019
+ *                                            *_____*
+ *                                           *_*****_*
+ *                                          *_(O)_(O)_*
+ *                                         **____V____**
+ *                                         **_________**
+ *                                         **_________**
+ *                                          *_________*
+ *                                           ***___***
+ */
+
+class PriorityQHeap(size: Int) : Heap(size) {
+
+    fun peek() = remove()
+}
---
+++ src/test/kotlin/chapter12/PriorityQHeap.kt
@@ -0,0 +1,41 @@
+package chapter12
+
+import org.junit.Before
+import org.junit.Test
+import java.lang.Math.random
+
+/* 
+ * Created with passion and love
+ *    for project DataStructuresAndAlgorithmsInJava(Lafore)
+ *        by Jatzuk on 16.05.2019
+ *                                            *_____*
+ *                                           *_*****_*
+ *                                          *_(O)_(O)_*
+ *                                         **____V____**
+ *                                         **_________**
+ *                                         **_________**
+ *                                          *_________*
+ *                                           ***___***
+ */
+
+class PriorityQHeapTest {
+    private val queueSize = 10
+    private val queue = PriorityQHeap(queueSize)
+
+    @Before
+    fun setUp() {
+        with(queue) {
+            repeat(queueSize) { insert((random() * 100).toInt()) }
+        }
+    }
+
+    @Test
+    fun priorityQHeapTest() {
+        with(queue) {
+            repeat(queueSize / 2) {
+                displayHeap()
+                peek()
+            }
+        }
+    }
+}
